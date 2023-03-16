9cc67a11659b47f3204bc6bc9abe691e71814fb6
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-03-16 19:37:52 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,59 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-function ToDo() {
-  return (<tr>
-    <td>
-      <p>id</p>
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <p>createdAt</p>
-    </td>
-  </tr>)
+function ToDo({ todos, setTodos }) {
+  const addInput = (e, i) => {
+    todos[i].value = e.target.value;
+    setTodos([...todos]);
+  };
+
+  return todos.map((task, i) => (
+    <tr key={i}>
+      <td>
+        <p>{task.id}</p>
+      </td>
+      <td>
+        <input value={task.value} onChange={(e) => addInput(e, i)} />
+      </td>
+      <td>
+        <p>{task.createdAt}</p>
+      </td>
+    </tr>
+  ));
 }
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 'todo1',
-    createdAt: '20:30',
-  }, {
-    id: 'todo2',
-    createdAt: '18:00',
-  }
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      value: "",
+      createdAt: "20:30",
+    },
+    {
+      id: "todo2",
+      value: "",
+      createdAt: "18:00",
+    },
   ]);
+
+  const reverseOrder = () => {
+    let revArr = [];
+    while (todos.length != 0) {
+      revArr.push(todos.pop());
+    }
+    setTodos(revArr);
+  };
 
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={reverseOrder}>Reverse</button>
       <table>
         <tbody>
+          <ToDo todos={todos} setTodos={setTodos} />
         </tbody>
       </table>
     </div>
-  )
+  );
 }
-
 
 export default App;
